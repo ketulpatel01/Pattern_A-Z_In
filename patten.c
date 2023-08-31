9eb2376d36fff5405df2676bc69ef3c473bb380b
ketulpatel01 <ketul3168@gmail.com>
2023-08-31 19:00:03 +0530

    A to Z pattern in c language

---
+++ patten.c
@@ -0,0 +1,188 @@
+#include <stdio.h>
+int main()
+{
+    //Input=> a
+ 	printf("    *\n");
+    printf("   * *\n");
+    printf("  * * *\n");
+    printf(" *     *\n");
+   
+
+//Input=> B
+    printf("* **\n");
+    printf("*   *\n");
+    printf("* * *\n");
+    printf("*   *\n");
+    printf("* **\n");
+
+//Input=> C
+    printf("* * *\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("* * *\n");
+
+//Input=> D
+    printf("* **\n");
+    printf("*   *\n");
+    printf("*   *\n");
+    printf("*   *\n");
+    printf("* **\n");
+
+//Input=> E
+    printf("* * *\n");
+    printf("*\n");
+    printf("* * *\n");
+    printf("*\n");
+    printf("* * *\n");
+
+//Input=> F
+    printf("* * *\n");
+    printf("*\n");
+    printf("* * *\n");
+    printf("*\n");
+    printf("*\n");
+
+//Input=> G
+    printf("* * * *\n");
+    printf("*   \n");
+    printf("*   * *\n");
+    printf("*     *\n");
+    printf("* * * *\n");
+
+//Input=> H
+    printf("*   *\n");
+    printf("*   *\n");
+    printf("* * *\n");
+    printf("*   *\n");
+    printf("*   *\n");
+
+//Input=> I
+    printf("* * *\n");
+    printf("  *\n");
+    printf("  *\n");
+    printf("  *\n");
+    printf("* * *\n");
+
+//Input=> J
+    printf("* * * *\n");
+    printf("    *\n");
+    printf("    *\n");
+    printf("*   *\n");
+    printf("* * *\n");
+
+//Input=> K
+    printf("*   *\n");
+    printf("*  *\n");
+    printf("* *\n");
+    printf("*  *\n");
+    printf("*   *\n");
+
+//Input=> L
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("*\n");
+    printf("* * *\n");
+
+//Input=> M
+    printf("    *       *\n");
+    printf("   * *     * *\n");
+    printf("  *   *   *   *\n");
+    printf(" *     * *     *\n");
+    printf("*       *       *\n");
+
+//Input=> N
+    printf("*     *\n");
+    printf("* *   *\n");
+    printf("*  *  *\n");
+    printf("*   * *\n");
+    printf("*     *\n");
+
+//Input=> O
+    printf(" ***\n");
+    printf("*   *\n");
+    printf("*   *\n");
+    printf("*   *\n");
+    printf(" ***\n");
+
+//Input=> P
+    printf("* * *\n");
+    printf("*   *\n");
+    printf("*  *\n");
+    printf("* *\n");
+    printf("*\n");
+    printf("*\n");
+
+//Input=> Q
+    printf(" ***\n");
+    printf("*   *\n");
+    printf("*   *\n");
+    printf("*   *\n");
+    printf(" ***\n");
+    printf("   *\n");
+
+//Input=> R
+    printf("* * *\n");
+    printf("*  *\n");
+    printf("* *\n");
+    printf("*  *\n");
+    printf("*   *\n");
+
+//Input=> S
+    printf("* * *\n");
+    printf("*\n");
+    printf("* * *\n");
+    printf("    *\n");
+    printf("* * *\n");
+
+//Input=> T
+    printf("* * *\n");
+    printf("  *\n");
+    printf("  *\n");
+    printf("  *\n");
+    printf("  *\n");
+
+//Input=> U
+    printf("*   *\n");
+    printf("*   *\n");
+    printf("*   *\n");
+    printf("*   *\n");
+    printf(" ***\n");
+
+//Input=> V
+    printf("*      *\n");
+    printf(" *    *\n");
+    printf("  *  *\n");
+    printf("   * \n");
+
+//Input=> W
+    printf("*       *       *\n");
+    printf(" *     * *     *\n");
+    printf("  *   *   *   *\n");
+    printf("   * *     * *\n");
+    printf("    *       *\n");
+
+//Input=> X
+    printf("*   *\n");
+    printf(" * *\n");
+    printf("  *\n");
+    printf(" * *\n");
+    printf("*   *\n");
+
+//Input=> Y
+    printf("*     *\n");
+    printf(" *   *\n");
+    printf("  * *\n");
+    printf("   *\n");
+    printf("   *\n");
+    printf("   *\n");
+
+//Input=> Z
+    printf("* * *\n");
+    printf("   *\n");
+    printf("  *\n");
+    printf(" *\n");
+    printf("* * *\n");
+    return 0;
+}
